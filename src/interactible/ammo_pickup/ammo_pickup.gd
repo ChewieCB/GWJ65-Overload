c856d2cbed440e6a2c60d4ae865b5b9577ca19f8
Jack McCaffrey <jackmccaffrey96@gmail.com>
2024-01-23 22:46:22 +0000

    Fixed ammo pickup mesh colours

--- src/interactible/ammo_pickup/ammo_pickup.gd
+++ src/interactible/ammo_pickup/ammo_pickup.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node3D
 
 @onready var ammo_meshes = [
@@ -5,10 +6,9 @@ extends Node3D
 	$Meshes/AmmoMesh2,
 	$Meshes/AmmoMesh3
 ]
-@onready var ammo_red_tex = load("res://assets/shotgun/Ammo_Textures/Ammo.png")
-@onready var ammo_yellow_tex = load("res://assets/shotgun/Ammo_Textures/ammo_yellow.png")
-@onready var ammo_blue_tex = load("res://assets/shotgun/Ammo_Textures/ammo_blue.png")
-@onready var ammo_textures = [ammo_red_tex, ammo_blue_tex, ammo_yellow_tex]
+@onready var ammo_red = $Meshes/Red
+@onready var ammo_yellow = $Meshes/Yellow
+@onready var ammo_blue = $Meshes/Blue
 
 @onready var counter_label = $Meshes/CounterLabel
 @export var ammo_amount: int = 1
@@ -33,15 +33,25 @@ enum AMMO_TYPE {
 @export var ammo_type: AMMO_TYPE = AMMO_TYPE.RED:
 	set(value):
 		ammo_type = value
-		if ammo_meshes:
-			for _mesh in ammo_meshes:
-				_mesh.texture.albedo_texture = ammo_textures[ammo_type]
+		if ammo_red and ammo_blue and ammo_yellow:
+			match ammo_type:
+				AMMO_TYPE.RED:
+					ammo_red.visible = true
+					ammo_blue.visible = false
+					ammo_yellow.visible = false
+				AMMO_TYPE.BLUE:
+					ammo_red.visible = false
+					ammo_blue.visible = true
+					ammo_yellow.visible = false
+				AMMO_TYPE.YELLOW:
+					ammo_red.visible = false
+					ammo_blue.visible = false
+					ammo_yellow.visible = true
 
 
 func _ready():
+	ammo_type = ammo_type
 	counter_label.text = str(ammo_amount)
-	for _mesh in ammo_meshes:
-		_mesh.get_active_material(0).albedo_texture = ammo_textures[ammo_type]
 
 
 func _physics_process(delta):
